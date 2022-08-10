47a30dbad605199610ff43cae916289e2674892a
nicolas <nicolas.moraeslsilva@gmail.com>
2022-08-10 18:16:40 -0300

    add: Adicionada prop required.

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,9 +5,10 @@ interface Props {
   name: string
   label: string
   type?: string
+  required?: boolean
 }
 
-const Input: React.FunctionComponent<Props> = ({ name, label, type }) => {
+const Input: React.FunctionComponent<Props> = ({ name, label, type, required }) => {
   const [value, setValue] = useState('')
 
   return (
@@ -17,6 +18,7 @@ const Input: React.FunctionComponent<Props> = ({ name, label, type }) => {
         name={name}
         onChange={(e) => { setValue(e.target.value) }}
         className={type == 'textarea' ? 'isTextArea' : ''}
+        required={required ? true : false}
       />
       <label className={value && 'filled'} htmlFor={name}>{label}</label>
     </Container>
