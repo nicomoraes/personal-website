555a008d2b29e379b1235934514d1a6f2f78fb05
nicolas <nicolas.moraeslsilva@gmail.com>
2022-08-09 21:25:58 -0300

    change: Mudanças em alguns estilos.

--- src/styles/portifolio.ts
+++ src/styles/portifolio.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display:  flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
   padding: 0 30%;
 
   @media(max-width: ${({theme}) => theme.breakpoints.md}){
-    padding: 10%;
+    padding: 0 10%;
   }
 `
 export const RepoContainer = styled.div`
@@ -18,14 +18,16 @@ export const RepoContainer = styled.div`
 export const Title = styled.h1`
   width: 100%;
   text-align: center;
-  margin-top: 100px;
+  margin-top: 50px;
   font-size: ${({theme}) => theme.fontSize.large.lg_300};
   color: ${({theme}) => theme.colors.primary};
   text-shadow: 1px 1px 1px ${({theme}) => theme.colors.tertiary};
+  
   @media(max-width: ${({theme}) => theme.breakpoints.md}){
     font-size: ${({theme}) => theme.fontSize.large.lg_300};
   }
-  @media(max-width: ${({theme}) => theme.breakpoints.md}){
+  @media(max-width: ${({theme}) => theme.breakpoints.sm}){
+    margin-top: 0;
     font-size: ${({theme}) => theme.fontSize.large.lg_200};
   }
 `
