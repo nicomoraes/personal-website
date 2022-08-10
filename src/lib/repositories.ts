c94a2e829011564ffe1e98b7b887d7e69347c851
nicolas <nicolas.moraeslsilva@gmail.com>
2022-08-10 18:18:30 -0300

    change: Correção em algumas descrições.

--- src/lib/repositories.ts
+++ src/lib/repositories.ts
@@ -1,4 +1,5 @@
 export interface RepositoryProps {
+  id: number
   name: string,
   description: string,
   url: string
@@ -6,18 +7,21 @@ export interface RepositoryProps {
 
  const repositories: RepositoryProps[] = [
   {
+    id: 1,
     name: 'Gerenciador de Tarefas',
-    description: 'Desenvolvido com o Framework Next.js e tem como armazenamendo o banco de dados MongoDB. Permite a adição, exclusão e edição de tarefas. Mantèm os dados atualizados utilizando a biblioteca SWR.',
+    description: 'Desenvolvido com o Framework Next.js e tem como armazenamendo o banco de dados MongoDB. Permite a adição, exclusão e edição de tarefas. Mantèm os dados atualizados entre o cliente e o banco de dados utilizando a biblioteca SWR.',
     url: 'https://github.com/nicomoraes/manager-task'
   },
   {
+    id: 2,
     name: 'Site pessoal',
-    description: 'O site que você acessa no atual momento. É construido com Next.js + typescript e utiliza a biblioteca styled-components para estilização.',
+    description: 'O site que você acessa no atual momento. É construido com Next.js + Typescript e utiliza a biblioteca styled-components para estilização.',
     url: 'https://github.com/nicomoraes/personal-website'
   },
   {
+    id: 3,
     name: 'IFSC',
-    description: 'Repositório que contém uma série de projetos desenvolvimentos durante minha formação em Análise e Desenvolvimento de Sistemas no Instituto Federal de Santa Catarina',
+    description: 'Repositório que contém uma série de projetos desenvolvidos durante minha formação em Análise e Desenvolvimento de Sistemas no Instituto Federal de Santa Catarina.',
     url: 'https://github.com/nicomoraes/ifsc-projects'
   }
 ]
